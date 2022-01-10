3d2400737a61aa4daabca0fc0a99cb23319d90da
denys <gleb.fomichov@gmail.com>
2022-01-10 18:14:25 +0200

    1

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import logging
 import telebot
 
+
+
 import config
 import dbworker
 
-# Объект бота
-bot = telebot.TeleBot("5084341118:AAFHCBMJhqUxDBB2RKrkIPgQODbcnzROIHY")
+token = '5084341118:AAFHCBMJhqUxDBB2RKrkIPgQODbcnzROIHY'
+
+bot = telebot.TeleBot(token)
 # Включаем логирование, чтобы не пропустить важные сообщения
-#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 messages=[]
 mass=['Адрес доставки',"Имя"]
 user_id=''
 
-#@bot.update_rules(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start_message(message):
     global user_id
     user_id=message.chat.id
-    keyboard = telebot.ReplyKeyboardMarkup(one_time_keyboard=True)
-    button_1 = telebot.KeyboardButton(text="Присоединиться")
+    keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    button_1 = telebot.types.KeyboardButton(text="Присоединиться")
     keyboard.add(button_1)
     button_2 = "Связь с человеком"
     keyboard.add(button_2)
@@ -27,20 +30,20 @@ def start_message(message):
 
 user=config.Users(id=user_id,status=config.States.S_ENTER_NAME.value)
 print(user.id)
-# def data_input(text):
-#     return f"Введите {text}"
-#
-#
-# @dp.message_handler()
-# async def any_text_message2(message: types.Message):
-#     if message.text=="Присоединиться":
-#         global mass
-#         await message.answer(data_input(mass[0]))
-#
-# @dp.message_handler(commands="reset")
-# async def cmd_reset(message:types.Message):
-#     dbworker.set_status(message.chat.id, config.States.S_ENTER_NAME.value)
-#     await message.answer( "Что ж, начнём по-новой. Как тебя зовут?")
+def data_input(text):
+    return f"Введите {text}"
+
+
+@bot.message_handler()
+def any_text_message2(message: telebot.types.Message):
+    if message.text=="Присоединиться":
+        global mass
+        bot.send_message(message.chat.id,data_input(mass[0]))
+
+@bot.message_handler(commands=["reset"])
+def cmd_reset(message:telebot.types.Message):
+    dbworker.set_status(message.chat.id, config.States.S_ENTER_NAME.value)
+    bot.send_message(message.chat.id,f"Данные сброшены.{data_input(mass[0])}")
 # @dp.message_handler(commands="test1")
 # async def cmd_test1(message: types.Message):
 #     await message.reply("Test 1")
@@ -52,6 +55,6 @@ print(user.id)
 
 if __name__ == "__main__":
     # Запуск бота
-    executor.start_polling(dp, skip_updates=True)
+    bot.polling()
 
 
